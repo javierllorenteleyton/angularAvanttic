53766b5dfcc9e67501727d4ee5db2a72d1aac358
AVTP35\llorente <javier.llorente@avanttic.com>
2018-05-30 19:35:50 +0200

    alumono

--- src/app/alumno/alumno.component.ts
+++ src/app/alumno/alumno.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Alumno } from './Alumno';
+import { AlumnoService } from './alumno.service';
+
 @Component({
   selector: 'app-alumno',
   templateUrl: './alumno.component.html',
-  styleUrls: ['./alumno.component.css']
+  styleUrls: ['./alumno.component.css'],
+  providers: [AlumnoService]
 })
 export class AlumnoComponent implements OnInit {
 
-  constructor() { }
+  public listAlumnos: Array<Alumno>;
+
+  constructor(private alumnoservice: AlumnoService) { }
 
   ngOnInit() {
+     this.refreshAlumnos();
+  }
+
+  private refreshAlumnos() {
+    this.listAlumnos= this.alumnoservice.getAlumnosList();
   }
 
 }
---
+++ src/app/alumno/alumno.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Alumno } from './Alumno';
+
+@Injectable()
+export class AlumnoService {
+   
+     private listAlumnos: Array<Alumno>;
+
+     constructor() {}
+
+     public getAlumnosList(): Array<Alumno> {
+         return this.mockAlumnos();
+     }
+
+     private mockAlumnos() : Array<Alumno> {
+         this.listAlumnos = [
+            new Alumno('Simon','I','111111'), 
+            new Alumno('Simon','II','111112'),
+            new Alumno('Simon','III','111113'),
+            new Alumno('Juan','I','111114'),
+            new Alumno('Mateo','I','111115'),
+            new Alumno('Carlos','I','111116'),
+            new Alumno('Pedro','I','111117'),
+            new Alumno('Santiago','II','111118'),  
+            new Alumno('Pedro','II','111119'),
+            new Alumno('Santiago','V','111120')                
+         ];
+         return this.listAlumnos;
+     }
+}
